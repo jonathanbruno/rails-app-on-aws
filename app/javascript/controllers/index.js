5cee258eee14e7a4e7cc02f6b90eb943755a2b23
Jonathan <jonathanbruno.ueg@gmail.com>
2024-11-15 21:45:29 -0300

    save

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
+import { hello_controller } from "controllers/hello_controller"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+eagerLoadControllersFrom("controllers", hello_controller)
