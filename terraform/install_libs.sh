144961bbf236ea2a17bf5f92a3f70b23681fd2da
Jonathan <jonathanbruno.ueg@gmail.com>
2024-12-27 17:47:24 -0300

    save

--- terraform/install_libs.sh
+++ terraform/install_libs.sh
@@ -10,6 +10,6 @@ sudo usermod -aG docker ec2-user
 
 sudo chmod 666 /var/run/docker.sock
 
-sudo docker pull
+sudo docker pull jonathanbruno/rails-aws-app:latest
 
-sudo docker run -d -p 3000:3000 -e CLOUD_FRONT_PREFIX=d9kl1g9ejh2nj --pull always jonathanbruno/rails-aws-app
+sudo docker run -d -p 3000:3000 -e CLOUD_FRONT_PREFIX=dxpvskz7l6s4f --pull always jonathanbruno/rails-aws-app
